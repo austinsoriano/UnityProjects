baf46654f205b15739db54ca91eb8e1624a0476c
Austin Soriano <Austin@D-10-156-8-115.dhcp4.washington.edu>
2015-02-03 13:31:55 -0800

    Re-organized Directory

---
+++ Tutorials/Tutorial4/Assets/Standard Assets/Physic Materials/CameraScript.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+function Start () {
+	var floor = GameObject.CreatePrimitive(PrimitiveType.Plane);
+	floor.transform.position = Vector3(0, -1, 10);
+	floor.transform.localScale = Vector3(10, 1, 10);
+	
+	var cube1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+	cube1.transform.position = Vector3(0, 10, 10);
+	cube1.transform.rotation = Quaternion.Euler(10, 0, 10);
+	cube1.AddComponent("CollisionColor");
+	cube1.AddComponent("Rigidbody");
+	cube1.collider.material.bounciness = 1.5;
+	
+	var cube2 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+	cube2.transform.position = Vector3(2, 10, 10);
+	cube2.AddComponent("CollisionColor");
+	cube2.AddComponent("Rigidbody");
+	cube2.collider.material.bounciness = 0;	
+}
---
+++ Tutorials/Tutorial4/Assets/Standard Assets/Physic Materials/CollisionColor.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+var theColor : Color;
+var theOtherColor : Color;
+
+function Start () {
+	theColor.r = 0;
+	theColor.g = 10;
+	theColor.b = 10;
+	
+	theOtherColor.r = 10;
+	theOtherColor.g = 0;
+	theOtherColor.g = 10;
+}
+
+function OnCollisionEnter() {
+	Debug.Log("Collided!");
+	if(renderer.material.color == theColor) {
+		renderer.material.color = theOtherColor;
+	} else {
+		renderer.material.color = theColor;
+	}
+}
